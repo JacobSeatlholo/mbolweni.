7c8cce932e7289ca5f3f7bc479c68d63533f0103
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-07-17 21:08:26 +0300

    update ui

--- app/components/hero-section.tsx
+++ app/components/hero-section.tsx
@@ -15,11 +15,11 @@ import Link from "next/link";
 const HeroSection = () => {
   return (
     <div className="h-screen w-full flex justify-start relative px-10">
-      <div className="absolute top-40">
+      <div className="absolute top-40 md:top-60">
         <h1 className="text-[2.6rem] leading-loose font-semibold font-montserrat">
           Order food to your door
         </h1>
-        <div className="flex md:space-x-2  py-4 md:w-full md:flex-row flex-col  items-baseline space-y-2 w-[500px]">
+        <div className="flex md:space-x-2  py-4 md:w-full md:flex-row flex-col  items-baseline space-y-2 ">
           <div className="flex-1  md:w-[500px]  flex bg-white justify-center items-center hover:border-b-2 hover:border-black  shadow-sm ">
             <HiLocationMarker size={20} className="ml-3" />
             <Input type="text" placeholder="Enter delivery address" />
